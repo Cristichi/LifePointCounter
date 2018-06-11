691e4a9107315616cdb7447297a738612bc01f20
Cristichi <cristichi@hotmail.es>
2018-06-11 12:33:24 +0200

    Esperemos que funcione

---
+++ app/src/main/java/com/cristichi/jony/lifepointcounter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cristichi.jony.lifepointcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+    HOLAAAAAA
+    JELOUUUUUUUUUUUUUUUUU
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
